0b9e5e9107e1a7f0ebcc3d6767171f7dd7f0f5c7
Amosram <amosrama733@gmail.com>
2024-03-16 01:43:32 +0300

    first commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let menuIcon = document.querySelector('#menu-icon');
-let sideBar = document.querySelector('.sidebar');
-
-
-menuIcon.onclick = () => {
-    sideBar.classList.toggle("active");
-}
-
-
